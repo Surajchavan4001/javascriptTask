863ee754e9f676ed727368315914479a6ede8d08
Suraj Chavan <surajchavan.0595@gmail.com>
2022-07-22 17:56:24 +0530

    object in js

---
+++ object.js
@@ -0,0 +1,52 @@
+// const person = {name:'Harsh',age:23, hobbies:["guitar","sleeping"]};
+// // console.log(person.hobbies[0]);
+// // person.gender="male";
+// // console.log(person)
+
+// // for in loop
+// //object key
+
+// for(let key in person){
+//     console.log(key,person[key]);
+// }
+
+//computed properties
+// const key1 = "objkey1";
+// const key2 = "objkey2";
+//  const value1 ="myvalue1";
+//  const value2 ="myvalue2";
+
+//  const obj ={
+//     [key1]:value1,
+//     [key2]:value2
+//  }
+//  console.log(obj);
+
+// spread opertor
+//  const obj1 ={
+//     key1: "value1",
+//     key2:"value2",
+//  }
+//  const obj2 ={
+//     key3: "value3",
+//     key4:"value4",
+//  }
+//  const newObj ={...obj1,...obj2};
+//  console.log(newObj);
+
+// object inside array
+// const users =[
+//     {userId:1,firstName: "suraj",gender:"male"},
+//     {userId:2,firstName: "sai",gender:"male"},
+//     {userId:3,firstName: "raj",gender:"male"},
+// ]
+// console.log(users);
+// for(let user of users){
+//     console.log(user);
+// }
+// for(let user of users){
+//     console.log(user.userId);
+// }
+// for(let user of users){
+//     console.log(user.firstName);
+// }
